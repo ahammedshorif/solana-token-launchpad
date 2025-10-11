8e6c4c654e4b22cd533678261e056b8f10c8e2cc
Ahammed Shorif <ahammedshorif20@gmail.com>
2025-10-12 00:30:39 +0600

    added connection and wallet Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
 import CreateToken from './components/CreateToken'
+import {ConnectionProvider, WalletProvider} from '@solana/wallet-adapter-react'
+import { WalletModalProvider,WalletDisconnectButton,WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import '@solana/wallet-adapter-react-ui/styles.css'
 
 function App() {
  
 
   return (
-    <CreateToken></CreateToken>
+    <ConnectionProvider endpoint="http://api.devnet.solana.com">
+      <WalletProvider wallets={[]}>
+
+          <div className="flex justify-between p-3 bg-neutral-950 sticky top-0">
+             <WalletMultiButton />
+             <WalletDisconnectButton />
+          </div>
+         <CreateToken></CreateToken>
+
+
+       </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
